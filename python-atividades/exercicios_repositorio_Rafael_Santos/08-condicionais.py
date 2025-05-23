981451638d1e2949f84fdea4498c1ad2c6067ee0
Nicole Silva <nicolesilva8144@gmail.com>
2025-05-23 12:15:04 -0300

    feat: Tarefa 8 do repositório do professor Rafael Santos

--- python-atividades/exercicios_repositorio_Rafael_Santos/08-condicionais.py
+++ python-atividades/exercicios_repositorio_Rafael_Santos/08-condicionais.py
@@ -1,8 +1,13 @@
+# 23/05 - Nicole Silva
+
 idade = 18
 
 if idade >= 18:
-    print("Você é um adulto")
+    print("Você é um adulto \n")
 else:
-    print("Você é menor de idade")
+    print("Você é menor de idade \n")
 
 #Siga o modelo acima e cria uma nova expressão condicional
+
+if idade <=  60:
+    print("Você pode ser classificado como pessoa do grupo de risco \n")
