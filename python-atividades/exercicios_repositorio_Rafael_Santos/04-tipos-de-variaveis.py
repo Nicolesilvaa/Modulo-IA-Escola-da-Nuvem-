844e0b56c670b0107f0b4713488b788b80fdf20a
Nicole Silva <nicolesilva8144@gmail.com>
2025-05-23 11:44:51 -0300

    feat: Tarefa 4 do repositório do professor Rafael Santos

--- python-atividades/exercicios_repositorio_Rafael_Santos/04-tipos-de-variaveis.py
+++ python-atividades/exercicios_repositorio_Rafael_Santos/04-tipos-de-variaveis.py
@@ -2,6 +2,12 @@
 Imprima na tela o tipo de cada variável abaixo, 
 depois mude o tipo de cada uma e verifique o novo tipo
 """
+#Data 23/05 - Nicole Silva
 
 x = 10
 y = "Dez"
+
+typeX = str(type(x))
+typeY =  str(type(y))
+
+print("\nEsse é o tipo das variaveis x e y, respectivamente: " + typeX + " " + typeY + "\n")
