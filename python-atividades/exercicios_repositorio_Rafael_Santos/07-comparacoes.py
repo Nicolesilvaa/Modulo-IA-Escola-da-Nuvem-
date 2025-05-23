dba6d0e9e78a8ca7c43a361b9fbf6a684a4a0811
Nicole Silva <nicolesilva8144@gmail.com>
2025-05-23 12:04:17 -0300

    feat: Tarefa 7 do repositório do professor Rafael Santos

--- python-atividades/exercicios_repositorio_Rafael_Santos/07-comparacoes.py
+++ python-atividades/exercicios_repositorio_Rafael_Santos/07-comparacoes.py
@@ -2,13 +2,17 @@
 
 x = 10
 y = 15
+
+print("\n")
 print(x > y, x < y, x == y, x != y)
 
 #Compare dois números e exiba uma frase na tela de acordo com a comparação escolhida
 
 if x > y:
-    print("x é maior que y")
+    print("\n x é maior que y")
 elif x < y:
-    print("x é menor que y")
+    print("\n x é menor que y")
 else:
-    print("x é igual a y")
+    print("\n x é igual a y")
+
+print()
