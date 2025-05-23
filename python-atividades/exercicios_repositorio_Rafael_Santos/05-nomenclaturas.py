27b03d279f73e7adad586efa28e331d8aa202460
Nicole Silva <nicolesilva8144@gmail.com>
2025-05-23 12:01:17 -0300

    feat: Tarefa 6 do repositório do professor Rafael Santos

--- python-atividades/exercicios_repositorio_Rafael_Santos/05-nomenclaturas.py
+++ python-atividades/exercicios_repositorio_Rafael_Santos/05-nomenclaturas.py
@@ -1,7 +1,6 @@
 #Corrija o nome das variaveis abaixo seguindo as boas práticas
 #Data 23/05 - Nicole Silva
 
-
 user_name = "Bob"
 user_name_2 = "Zorro"
 middle_name = "Fernandes"
--- python-atividades/exercicios_repositorio_Rafael_Santos/06-operadores-matematicos.py
+++ python-atividades/exercicios_repositorio_Rafael_Santos/06-operadores-matematicos.py
@@ -1,9 +1,23 @@
+#Data 23/05 - Nicole Silva
+
 a = 10
 b = 3
 
+print("\n")
 print(a + b, a - b, a * b, a / b, a ** b, a // b, a % b)
 
 """
 Crie quatro novas variaveis de numeros inteiros, some elas, 
 calcule a média e exiba o resultado na tela
 """
+
+c = 7
+d = 15
+e = 12
+f = 6
+
+soma = c + d + e + f
+media = soma / 4
+
+print("\nA soma é:", soma)
+print("\nA média é:", media, "\n")
