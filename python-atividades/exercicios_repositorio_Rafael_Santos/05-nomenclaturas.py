9e9cea2adff5019c040f9a91addc02daf51abb2f
Nicole Silva <nicolesilva8144@gmail.com>
2025-05-23 11:57:18 -0300

    feat: Tarefa 5 do repositório do professor Rafael Santos

--- python-atividades/exercicios_repositorio_Rafael_Santos/05-nomenclaturas.py
+++ python-atividades/exercicios_repositorio_Rafael_Santos/05-nomenclaturas.py
@@ -1,8 +1,8 @@
 #Corrija o nome das variaveis abaixo seguindo as boas práticas
 
 user_name = "Bob"
-UserName = "Zorro"  
-nomedomeio = "Fernandes"
+user_name_2 = "Zorro"
+middle_name = "Fernandes"
 
 
 
