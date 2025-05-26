9cd9520e6110293c87e2ba1d35f60878dbaaa8ab
Nicole Silva <nicolesilva8144@gmail.com>
2025-05-26 12:09:48 -0300

    feat: Atualizando tarefa do random e subindo tarefa do request

--- python-atividades/exercicios_modulos/criar_requestes.py
+++ python-atividades/exercicios_modulos/criar_requestes.py
@@ -3,14 +3,14 @@ import requests
 
 #Corzinha:
 AZUL = '\033[94m'
-
+BRANCO  = '\033[0m'
 
 resposta = requests.get('https://randomuser.me/api/?results=10&inc=name,email,nat')
 users = resposta.json()
 
 usuario = users["results"][0]
 
-print(f"{AZUL}Nome: {usuario['name']['first']} {usuario['name']['last']}{RESET}")
-print(f"{AZUL}Email: {usuario['email']}{RESET}")
-print(f"{AZUL}Nacionalidade: {usuario['nat']}{RESET}")
+print(f"{AZUL}Nome: {usuario['name']['first']} {usuario['name']['last']}")
+print(f"{AZUL}Email: {usuario['email']}")
+print(f"{AZUL}Nacionalidade: {usuario['nat']}")
 
